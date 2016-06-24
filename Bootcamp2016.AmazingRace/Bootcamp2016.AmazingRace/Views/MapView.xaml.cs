d501c3e3602d653033cee79f1efe378682c4badb
Jonathon Powell <jpowell@infusion.com>
2016-06-24 13:57:46 -0400

    created map view and hooked it up to the model (fingers crossed)

---
+++ Bootcamp2016.AmazingRace/Bootcamp2016.AmazingRace/Views/MapView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Bootcamp2016.AmazingRace.Views
+{
+    public partial class MapView : ContentPage
+    {
+        public MapView()
+        {
+            InitializeComponent();
+        }
+    }
+}
